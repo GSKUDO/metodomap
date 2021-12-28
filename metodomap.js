0cbe8b96c3c924d4c547a3ef1412a0666911a968
Gislene Sayuri <gskudo@yahoo.com.br>
2021-12-27 22:20:21 -0300

    Add files via upload

--- metodomap.js
+++ metodomap.js
@@ -1,13 +1,18 @@
 var clientes = [
 ];
+var clientesjuros = [
+];
 
+//cadastra os clientes
 function incluircliente(){
     clientes.push({
         'NomeCliente': document.getElementById("nome").value,
         'DataVencimento': document.getElementById("datavencimento").value,
-        'ValorCompra': document.getElementById("valor").value
+        'ValorCompra': document.getElementById("valor").value,
+        'ValorAtualizado': ""
     });
     
+    //mostra os clientes em uma tabela
     var tabela = clientes.map(function(item, indice){
         return `<tr>
                 <td>${indice + 1}</td>
@@ -20,41 +25,42 @@ function incluircliente(){
 }
 
 function funcaojuros(){
-    console.log("clientes antes" + clientes);
-    clientesjuros = clientes;
-    var tabjuros = clientesjuros.map(function(item, indice){
-        // One day Time in ms (milliseconds)
-        var one_day = 1000 * 60 * 60 * 24
-        // To set present_dates to two variables
-        var hoje = new Date();
-        console.log("hoje" + hoje);
-        var dataVence = new Date(item.DataVencimento);
-        console.log("datavenc" + dataVence);
-        // To calculate the time difference of two dates
-        var Difference_In_Time = hoje.getTime() - dataVence.getTime();
-        console.log("tempo" + Difference_In_Time);
-        if (Difference_In_Time > 0){
-            // To calculate the no. of days between two dates
-	        var diasAtraso = Difference_In_Time / (1000 * 3600 * 24);
-            console.log("Dias de Atraso" + diasAtraso);
-            juros = diasAtraso * 0.001 * parseFloat(item.ValorCompra);
-            console.log("juros" + juros);
-            mora = parseFloat(item.ValorCompra) * 0.02;
-            console.log("mora" + mora);
-        } else {
-            juros = 0;
-            mora = 0;
-        }     
-        item.ValorCompra = parseFloat(item.ValorCompra) + juros + mora;
-        console.log("item.ValorCompra" + item.ValorCompra);
-        console.log("clientes" + clientes);
-        console.log("clientesjuros" + clientesjuros);
-    return `<tr>
+        //calcula os juros
+        var tabjuros = clientes.map(function(item, indice){
+            
+            // um dia em milissegundos
+            var one_day = 1000 * 60 * 60 * 24
+            
+            // cria variaveis com as datas
+            var hoje = new Date();
+            var dataVence = new Date(item.DataVencimento);
+        
+            // calcula a diferença entre datas
+            var Difference_In_Time = hoje.getTime() - dataVence.getTime();
+        
+            // verifica se está atrasado 
+            if (Difference_In_Time > 0){
+                // calcula o numero de dias entre as duas datas
+	            var diasAtraso = Difference_In_Time / (1000 * 3600 * 24);
+                console.log("Dias de Atraso" + diasAtraso);
+                // calcula juros
+                juros = diasAtraso * 0.001 * parseFloat(item.ValorCompra);
+                //calcula mora 
+                mora = parseFloat(item.ValorCompra) * 0.02;
+                // Dá o valor atualizado
+                item.ValorAtualizado = parseFloat(item.ValorCompra) + juros + mora;
+            }else{
+                item.ValorAtualizado = item.ValorCompra;
+            }     
+
+        // coloca os valores atualizados na tabela 
+        return `<tr>
             <td>${indice + 1}</td>
-            <td>${item.nome}</td>
+            <td>${item.NomeCliente}</td>
             <td>${item.DataVencimento}</td>
-            <td>${item.ValorCompra.toFixed(2)}</td>
+            <td>${item.ValorAtualizado}</td>
             </tr>`;
-    });
+        });
+
     document.querySelector("#tbjuros tbody").innerHTML = tabjuros.join("");
 }
